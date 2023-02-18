a2c4d9b115129c2cdc19dff2dbe9a2ef11380dfe
Lisbeth Ortega <lisbeth_ortega@ucne.edu.do>
2023-02-17 23:42:42 -0400

    Ejercicios del capitulo 10

---
+++ ejercicio2 Capitulo10/ejercicio2 Capitulon 10/Program.cs
@@ -0,0 +1,20 @@
+﻿//Crear una clase para llevar la información de los estudiantes de una escuela.
+
+using System.ComponentModel.DataAnnotations;
+
+public class Estudiantes
+{
+    [Key]
+    public int IdEstudiante { get; set; }
+    public string? Nombre { get; set; }
+    public decimal Promedio { get; set; }
+    public int Matricula { get; set; }
+
+    public Estudiantes(int id,string nombre,decimal promedio, int matricula) {
+        IdEstudiante = id;
+        Nombre = nombre;
+        Promedio = promedio;
+        Matricula = matricula;
+
+    }
+}
